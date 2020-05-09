4bb7de775fe9af2485ed934e1ddcd061a6dd47ee
ChiangPong <34678058+ChiangPong@users.noreply.github.com>
2020-05-09 22:37:13 +0200

    Sample To DO AP
    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';/* 
+import logo from './logo.svg'; */
+import './App.css';
+import Task from './components/Task';
+
+function App() {  
+  return (
+    <div className="container">
+      <Task />
+    </div>
+  );
+}
+
+export default App;
